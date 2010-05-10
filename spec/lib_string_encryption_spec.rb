9fbc15e1d86ad9d687839da0b486241a8bafc371
Jason Rogers <jacaetevha@gmail.com>
2010-05-10 09:53:53 -0400

    add tests for decryption failures

--- spec/lib_string_encryption_spec.rb
+++ spec/lib_string_encryption_spec.rb
@@ -2,10 +2,24 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe 'String Encryption' do
   it "should encrypt strings" do
-    "Hello World".should_not == "Hello World".encrypt 
+    "Hello World".should_not == "Hello World".encrypt
   end
-  
+
   it "should decrypt a string that's been encrypted" do
     "Hello World".encrypt.decrypt.should == "Hello World"
-  end 
+  end
+
+  it "should fail to decrypt a string encrypted with the wrong key" do
+    value = "Hello World".encrypt
+    original_encryption_string = ENV['LIB_STRING_ENCRYPTION_KEY']
+    begin
+      ENV['LIB_STRING_ENCRYPTION_KEY'] = StringEncryption::SecureRandom.hex(14)
+      "Hello World".encrypt.should_not == value
+      "Hello World".encrypt.decrypt.should == "Hello World"
+      lambda {value.decrypt}.should raise_error(OpenSSL::Cipher::CipherError)
+    ensure
+      ENV['LIB_STRING_ENCRYPTION_KEY'] = original_encryption_string
+      value.decrypt.should == "Hello World"
+    end
+  end
 end
